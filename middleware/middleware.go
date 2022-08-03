35d5e987c66378471d0e8bb4e34945eef0c2380d
elleven11 <federico.cassano@federico.codes>
2022-08-03 15:55:38 -0400

    logger

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,10 +1,36 @@
 package middleware
 
 import (
+	"io"
+	"log"
+	"os"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func Setup(app *fiber.App) {
-	app.Use(logger.New())
+	setupLogger(app)
+}
+
+func setupLogger(app *fiber.App) {
+	date := time.Now().String()
+	apiFile, err := os.OpenFile("./logs/api-log-"+date+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	fiberFile, err := os.OpenFile("./logs/fiber-log-"+date+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer apiFile.Close()
+	wrt := io.MultiWriter(os.Stdout, apiFile)
+	log.SetOutput(wrt)
+
+	loggerConfig := logger.Config{
+		Output: fiberFile,
+	}
+
+	app.Use(logger.New(loggerConfig))
 }
